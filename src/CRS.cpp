0aeab9526ac88d2cf47ab09f410372ce6b7f8e22
alisaleemhasan <ali1saleem1hasan@gmail.com>
2025-02-18 20:21:28 +0000

    create initial-simple convert to CRS method

--- src/CRS.cpp
+++ src/CRS.cpp
@@ -18,8 +18,21 @@ class CRM{
 		// convert sparce matrix to CRS format
 		// print origianl matrix
 		// multiplyMatrices
-		//
-		void convert(){}
+		void convert(){
+			row_ptrs.push_back(1); // TODO: know why it starts from 1 ??
+			row_ptrs.clear(); cols.clear(); vals.clear();
+			for(const auto& row :originalMatrix){
+				for(int col_idx=0;col_idx<row.size();col_idx++){
+					if(row[col_index]!=0.0){
+						cols.push_back(col_idx+1);
+						vals.push_back(row[col_idx]);
+					}
+					
+				}
+			row_ptrs.push_back(cols.size() + 1);
+			
+			}
+		}
 		void printOriginal(){}
 		CRM operator*(CRM const& m){}
 		void uvalue(int row, int col, int new_val){}//update value
