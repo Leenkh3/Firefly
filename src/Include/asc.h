b75d92fec6ddc16efee8f320a421734308f293ed
danield0ma <73988380+danield0ma@users.noreply.github.com>
2025-03-12 11:40:50 +0100

    Create asc.h file with class and initial methods

---
+++ src/Include/asc.h
@@ -0,0 +1,41 @@
+
+#ifndef ASC_READER_H
+#define ASC_READER_H
+
+#include <string>
+#include <vector>
+
+class ASCReader
+{
+public:
+    struct Coordinate
+    {
+        double x;
+        double y;
+        double z;
+
+        Coordinate(double x_val = 0.0, double y_val = 0.0, double z_val = 0.0);
+    };
+
+    struct Connectivity
+    {
+        int x;
+        int y;
+        int z;
+        int a;
+        int b;
+        int c;
+
+        Connectivity(int x_val = 0, int y_val = 0, int z_val = 0, int a_val = 0, int b_val = 0, int c_val = 0);
+    };
+
+    explicit ASCReader(const std::string &file_path);
+
+    bool readFile();
+private:
+    std::string filename;
+    std::vector<Coordinate> coordinates;
+    std::vector<Connectivity> connections;
+};
+
+#endif
