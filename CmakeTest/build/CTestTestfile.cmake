f8c520dc648b201d7a004f94098b6d4a12067b8a
vajkaimiklos <vajkaimiklos22@gmail.com>
2025-03-22 18:45:04 +0000

    Saving local vhanges before rebase

--- CmakeTest/build/CTestTestfile.cmake
+++ CmakeTest/build/CTestTestfile.cmake
@@ -1,8 +1,8 @@
 # CMake generated Testfile for 
-# Source directory: /work/students/hpc-f0v113/firefly/CmakeTest
-# Build directory: /work/students/hpc-f0v113/firefly/CmakeTest/build
+# Source directory: /work/students/hpc-f0v113/firefly/Firefly/CmakeTest
+# Build directory: /work/students/hpc-f0v113/firefly/Firefly/CmakeTest/build
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(RunGmshReader "/work/students/hpc-f0v113/firefly/CmakeTest/build/GmshReader" "example.msh")
-set_tests_properties(RunGmshReader PROPERTIES  _BACKTRACE_TRIPLES "/work/students/hpc-f0v113/firefly/CmakeTest/CMakeLists.txt;12;add_test;/work/students/hpc-f0v113/firefly/CmakeTest/CMakeLists.txt;0;")
+add_test(RunGmshReader "/work/students/hpc-f0v113/firefly/Firefly/CmakeTest/build/GmshReader" "../example.msh")
+set_tests_properties(RunGmshReader PROPERTIES  _BACKTRACE_TRIPLES "/work/students/hpc-f0v113/firefly/Firefly/CmakeTest/CMakeLists.txt;12;add_test;/work/students/hpc-f0v113/firefly/Firefly/CmakeTest/CMakeLists.txt;0;")
