e099fb98de2aa10eed3159f5f0d8d7e9ebc86854
Takacs Martin <originalmartin97@gmail.com>
2025-04-10 19:49:09 +0000

    Refactor main function in firefly.cpp and remove headers.h for cleaner code structure

--- firefly.cpp
+++ firefly.cpp
@@ -1,11 +1,7 @@
-#include "headers.h"
+#include <iostream>
 
-int main(int argc, char* argv[]) {
-    // Initialize the program
-    // This is where the main execution of the program starts.
-    // You can add any necessary initialization code here.
+int main() {
     std::cout << "Firefly Project - Main Execution" << std::endl;
-    processInput(argc, argv);
    
     return 0;
 }
--- headers.h
+++
@@ -1,3 +0,0 @@
-// All the headers necessary stored here.
-#include <iostream>
-#include "src/user_input/user_input.h"
