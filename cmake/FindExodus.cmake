04ae8e8965b1a7d0ddac88bd967354fe5880624d
kzoli39 <zoltavn@gmail.com>
2025-04-15 16:50:20 +0200

    FindExodus

--- cmake/FindExodus.cmake
+++ cmake/FindExodus.cmake
@@ -1,13 +1,32 @@
 FIND_PACKAGE(NetCDF REQUIRED)
+if(EXODUS_INCLUDE_DIRS AND EXODUS_LIBRARIES)
+  set(EXODUS_FIND_QUIETLY TRUE)
+endif()
 
-FIND_PATH(EXODUS_INCLUDE_DIR NAMES exodusII.h)
+find_path(EXODUS_INCLUDE_DIR NAMES exodusII.h
+        PATHS ${EXODUS_INSTALL_DIR}
+            $ENV{EXODUS_DIR}
+            ${CMAKE_BINARY_DIR}/exodus/install
+            /usr/include
+            /usr/local/include
+        PATH_SUFFIXES include)
 
-FIND_LIBRARY(EXODUS_LIBRARIES NAMES exodusii exodusIIv2c)
+set(EXODUS_INCLUDE_DIRS ${EXODUS_INCLUDE_DIR})
 
-INCLUDE(FindPackageHandleStandardArgs)
+find_library(EXODUS_LIBRARY NAMES exodus_for exodus
+            PATHS ${EXODUS_INSTALL_DIR}
+                   $ENV{EXODUS_DIR}
+                   ${CMAKE_BINARY_DIR}/exodus/install
+                   /usr/lib
+                   /usr/local/lib
+             PATH_SUFFIXES lib)
+
+set(EXODUS_LIBRARIES ${EXODUS_LIBRARY})
+
+include(FindPackageHandleStandardArgs)
 
 # handle the QUIETLY and REQUIRED arguments and set EXODUS_FOUND to TRUE if 
 # all listed variables are TRUE
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(Exodus DEFAULT_MSG EXODUS_LIBRARIES EXODUS_INCLUDE_DIR)
 
-MARK_AS_ADVANCED(EXODUS_INCLUDE_DIR EXODUS_LIBRARIES)
+MARK_AS_ADVANCED(EXODUS_INCLUDE_DIRS EXODUS_LIBRARIES)
