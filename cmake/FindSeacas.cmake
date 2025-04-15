92a78c2d32691b129871b74a021aa483da5ceb15
kzoli39 <zoltavn@gmail.com>
2025-04-15 16:26:50 +0200

    find seacas

--- cmake/FindSeacas.cmake
+++ cmake/FindSeacas.cmake
@@ -2,17 +2,19 @@ if(SEACAS_INCLUDE_DIRS AND SEACAS_LIBRARIES)
   set(SEACAS_FIND_QUIETLY TRUE)
 endif()
 
+# Search for headers
 find_path(SEACAS_INCLUDE_DIR SEACASIoss_config.h
           PATHS ${SEACAS_INSTALL_DIR}
                 $ENV{SEACAS_DIR}
                 ${CMAKE_BINARY_DIR}/seacas/install
-                /usr/include/seacas
-                /usr/local/include/seacas
+                /usr/include
+                /usr/local/include
           PATH_SUFFIXES include)
 
 set(SEACAS_INCLUDE_DIRS ${SEACAS_INCLUDE_DIR})
 
-find_library(SEACAS_LIBRARY NAMES seacas libseacas
+# Search for the main SEACAS library
+find_library(SEACAS_LIBRARY NAMES seacas libseacas.a
              PATHS ${SEACAS_INSTALL_DIR}
                    $ENV{SEACAS_DIR}
                    ${CMAKE_BINARY_DIR}/seacas/install
