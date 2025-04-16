758d9d23c1f7c98eb9dc37d2cc8e0e6790d4b812
kzoli39 <zoltavn@gmail.com>
2025-04-16 13:36:46 +0200

    findexodus try

--- cmake/FindExodus.cmake
+++ cmake/FindExodus.cmake
@@ -1,29 +1,40 @@
-if(EXODUS_INCLUDE_DIRS AND EXODUS_LIBRARIES)
-  set(EXODUS_FIND_QUIETLY TRUE)
-endif()
+# FindExodus.cmake
+# Module to locate the ExodusII library (from SEACAS)
 
-find_path(EXODUS_INCLUDE_DIR NAMES exodusII.h
-        PATHS ${EXODUS_INSTALL_DIR}
-            $ENV{EXODUS_DIR}
-            ${CMAKE_BINARY_DIR}/exodus/install
-            /usr/include
-            /usr/local/include
-        PATH_SUFFIXES include)
+# The following variables will be set:
+#   Exodus_FOUND         - True if Exodus was found
+#   Exodus_INCLUDE_DIRS  - The include directories for Exodus
+#   Exodus_LIBRARIES     - The Exodus library
 
-find_library(EXODUS_LIBRARY NAMES exodus libexodus_for.a
-            PATHS ${EXODUS_INSTALL_DIR}
-                   $ENV{EXODUS_DIR}
-                   ${CMAKE_BINARY_DIR}/exodus/install
-                   /usr/lib
-                   /usr/local/lib
-             PATH_SUFFIXES lib)
+find_path(Exodus_INCLUDE_DIR
+    NAMES exodusII.h
+    PATHS
+        ${EXODUS_ROOT}/include
+        /usr/include
+        /usr/local/include
+        /opt/local/include
+        ENV CPATH
+)
 
-set(EXODUS_LIBRARIES ${EXODUS_LIBRARY})
+find_library(Exodus_LIBRARY
+    NAMES exodus exoIIv2c
+    PATHS
+        ${EXODUS_ROOT}/lib
+        /usr/lib
+        /usr/local/lib
+        /opt/local/lib
+        ENV LIBRARY_PATH
+)
 
 include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Exodus
+    REQUIRED_VARS Exodus_LIBRARY Exodus_INCLUDE_DIR
+    VERSION_VAR Exodus_VERSION
+)
 
-# handle the QUIETLY and REQUIRED arguments and set EXODUS_FOUND to TRUE if 
-# all listed variables are TRUE
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Exodus DEFAULT_MSG EXODUS_LIBRARIES EXODUS_INCLUDE_DIR)
+if(Exodus_FOUND)
+    set(Exodus_INCLUDE_DIRS ${Exodus_INCLUDE_DIR})
+    set(Exodus_LIBRARIES ${Exodus_LIBRARY})
+endif()
 
-MARK_AS_ADVANCED(EXODUS_INCLUDE_DIR EXODUS_LIBRARIES)
+mark_as_advanced(Exodus_INCLUDE_DIR Exodus_LIBRARY)
