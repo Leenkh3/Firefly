2ebdfe428bd435a00325b57024b999ad7b6514dc
kzoli39 <zoltavn@gmail.com>
2025-04-23 09:43:45 +0200

    Debugging findexodus

--- cmake/FindExodus.cmake
+++ cmake/FindExodus.cmake
@@ -1,30 +1,43 @@
-if(EXODUS_INCLUDE_DIRS AND EXODUS_LIBRARIES)
-  set(EXODUS_FIND_QUIETLY TRUE)
+# FindExodus.cmake
+# Module to locate the ExodusII library (from SEACAS)
+
+# The following variables will be set:
+#   Exodus_FOUND         - True if Exodus was found
+#   Exodus_INCLUDE_DIRS  - The include directories for Exodus
+#   Exodus_LIBRARIES     - The Exodus library
+
+find_path(Exodus_INCLUDE_DIR
+    NAMES exodusII.h
+    PATHS
+        ${EXODUS_ROOT}/include
+        /usr/include
+        /usr/local/include
+        /opt/local/include
+        ENV CPATH
+)
+
+find_library(Exodus_LIBRARY
+    NAMES exodus exoIIv2c
+    PATHS
+        ${EXODUS_ROOT}/lib
+        /usr/lib
+        /usr/local/lib
+        /opt/local/lib
+        ENV LIBRARY_PATH
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Exodus
+    REQUIRED_VARS Exodus_LIBRARY Exodus_INCLUDE_DIR
+    VERSION_VAR Exodus_VERSION
+)
+
+if(Exodus_FOUND)
+    message(STATUS "Exodus found in FindExodus.cmake")
+    set(Exodus_INCLUDE_DIRS ${Exodus_INCLUDE_DIR})
+    set(Exodus_LIBRARIES ${Exodus_LIBRARY})
+else()
+    message(STATUS "Exodus NOT found in FindExodus.cmake")
 endif()
 
-find_path(EXODUS_INCLUDE_DIR exodusII.h
-          PATHS ${EXODUS_INSTALL_DIR}
-                $ENV{EXODUS_DIR}
-                ${CMAKE_BINARY_DIR}/exodus/install
-                /usr/lib/${CMAKE_LIBRARY_ARCHITECTURE}/exodus/mpi
-                /usr/local/include
-                /usr/local
-          PATH_SUFFIXES include)
-
-set(EXODUS_INCLUDE_DIRS ${EXODUS_INCLUDE_DIR})
-
-find_library(EXODUS_LIBRARY NAMES exodus
-             PATHS ${EXODUS_INSTALL_DIR}
-                   $ENV{EXODUS_DIR}
-                   ${CMAKE_BINARY_DIR}/exodus/install
-                   /usr/lib/${CMAKE_LIBRARY_ARCHITECTURE}/exodus/mpi
-                   /usr/local/lib
-                   /usr/local
-             PATH_SUFFIXES lib)
-
-set(EXODUS_LIBRARIES ${EXODUS_LIBRARY})
-
-include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Exodus DEFAULT_MSG EXODUS_INCLUDE_DIRS EXODUS_LIBRARIES)
-
-mark_as_advanced(EXODUS_INCLUDE_DIRS EXODUS_LIBRARIES)
+mark_as_advanced(Exodus_INCLUDE_DIR Exodus_LIBRARY)
